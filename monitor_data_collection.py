eec4f38110da845dd284e2d4b318b74e3d346ab0
Mark Broihier <mbroihier@yahoo.com>
2018-07-21 14:16:07 -0500

    make an all bytes line

--- monitor_data_collection.py
+++ monitor_data_collection.py
@@ -20,10 +20,7 @@ class MonitorLogToJson(object):
         self.file_handle = open(log_file, "r")
         self.output_file = open(plot_info_file, "w")
         self.series = {}
-        self.series["inbound traffic"] = []
-        self.series["inbound traffic total"] = []
-        self.series["outbound traffic"] = []
-        self.series["outbound traffic total"] = []
+        self.series["all"] = []
         self.is_forward = re.compile(r" FWD")
         self.is_inbound = re.compile(r"IN=wlan")
         self.is_outbound = re.compile(r"IN=usb")
@@ -55,26 +52,22 @@ class MonitorLogToJson(object):
         line = self.file_handle.readline().strip()
         incoming_total = 0
         outgoing_total = 0
+        all_bytes = 0
+        next_time_stamp = 0
         while line != "":
             if self.is_forward.search(line): # this is a trace with data being forwarded
                 match = self.packet_length.search(line)
                 packet_length = int(match.group(1))
+                all_bytes += packet_length
                 match = self.source.search(line)
                 source = match.group(1)
                 match = self.destination.search(line)
                 destination = match.group(1)
                 match = self.time_stamp.search(line)
                 time_stamp = float(match.group(1))
-                if self.is_inbound.search(line): #packet is coming into phone network
-                    incoming_total = incoming_total + packet_length
-                    self.series["inbound traffic"].append([time_stamp, packet_length])
-                    self.series["inbound traffic total"].append([time_stamp, incoming_total])
-                    print(match.group(1) + " " + source + " " + destination + ": " + str(packet_length) + " " + str(incoming_total))
-                else:
-                    outgoing_total = incoming_total + packet_length
-                    self.series["outbound traffic"].append([time_stamp, packet_length])
-                    self.series["outbound traffic total"].append([time_stamp, outgoing_total])
-                    print(match.group(1) + " " + source + " " + destination + ": " + str(packet_length) + " " + str(outgoing_total))
+                if time_stamp >= next_time_stamp:
+                    self.series["all"].append([time_stamp, all_bytes])
+                    next_time_stamp = time_stamp + 1.0
                 self.store_by_source_and_destination(source, destination, time_stamp, packet_length)
             line = self.file_handle.readline().strip()
 
